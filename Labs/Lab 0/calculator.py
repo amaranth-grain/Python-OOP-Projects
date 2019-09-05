88bc28219af3b09ad470ccbefff17de064eae898
Christy Yau <abstruse.amaranth@gmail.com>
2019-09-05 15:16:39 -0700

    Finished Lab 0

---
+++ Labs/Lab 0/calculator.py
@@ -0,0 +1,16 @@
+def Sum(a, b):
+    return a + b
+
+def Multiply(a, b):
+    return a * b
+
+def Divide(a, b):
+    return a / b
+
+def Subtract(a, b):
+    return a - b
+
+print(Sum(10, 5))
+print(Multiply(10, 5))
+print(Divide(10, 5))
+print(Subtract(10, 5))
---
+++ Labs/Lab 0/hypotenuse.py
@@ -0,0 +1,6 @@
+import math
+
+def CalculateHypotenuse(a, b):
+    return math.sqrt(a**2 + b**2)
+
+print(CalculateHypotenuse(3,4))

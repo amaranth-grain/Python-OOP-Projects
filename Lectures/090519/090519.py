1efdf2fff7a938ed5d53aeca3be6cbc4e0e04558
Christy Yau <abstruse.amaranth@gmail.com>
2019-09-05 16:31:32 -0700

    Lecture Notes for September 05, 2019

---
+++ Lectures/090519/090519.py
@@ -0,0 +1,10 @@
+def sum(a, b):
+    return a + b
+
+def main():
+    num1 = int(input("Enter first value: "))
+    num2 = int(input("Enter second value: "))
+    print(sum(num1, num2))
+
+if __name__ == "__main__":
+    main()

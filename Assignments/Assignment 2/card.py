77d4d0d277e2ba25c2afd55a1d110843e3601773
Christy Yau <abstruse.amaranth@gmail.com>
2019-10-06 04:45:18 -0700

    Implemented serialisation from Python object to JSON format

--- Assignments/Assignment 2/card.py
+++ Assignments/Assignment 2/card.py
@@ -2,8 +2,28 @@
 Module captures and represents most common card types for card management
 system.
 """
+import json
+from datetime import datetime
 from abc import ABC
 from enum import Enum
+
+
+class Address:
+    """
+    Represent mailing address.
+    """
+
+    def __init__(self, street, postal_code, city, province, country):
+        self.street = street
+        self.postal_code = postal_code
+        self.city = city
+        self.province = province
+        self.country = country
+
+    def __str__(self):
+        return f"{self.street}\n" \
+               f"{self.postal_code}\n" \
+               f"{self.city} {self.province} {self.country}"
 
 
 class CardType(Enum):
@@ -16,56 +36,46 @@ class CardType(Enum):
     AMEX = 4
 
 
-class Address:
-    """
-    Represent mailing address.
-    """
-    def __init__(self, street, postal_code, city, province, country):
-        self.street = street
-        self.postal_code = postal_code
-        self.city = city
-        self.province = province
-        self.country = country
-
-
 class Card(ABC):
-
-    _card_system_no = 1
-
     """
     Basic information for company-issued cards.
     """
-    def __init__(self, company_address, company_data):
+
+    def __init__(self, company_name, company_phone, company_site,
+                 company_address):
+        """
+        Initialise Card with basic company information
+        :param name: String
+        :param phone: String
+        :param site: String
+        :param company_address: Address
+        """
+        self._company_name = company_name
+        self._company_phone = company_phone
+        self._company_site = company_site
         self._company_address = company_address
-        self._company_data = company_data
 
-    @property
-    def company_data(self):
-        return self._company_data
-
-    @company_data.setter
-    def company_data(self, data):
-        self._company_data = data
-
-    @classmethod
-    def increment_id(cls):
-        cls._card_system_no += 1
-        return cls._card_system_no
-
-    def format_json_card(self):
-        return
+    def jsonfy(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          indent=4)
 
     def __str__(self):
-        return
+        return f"* {self._company_name.upper()} *\n" \
+               f"{self._company_address}\n" \
+               f"Tel: {self._company_phone}\n" \
+               f"Website: {self._company_site}"
 
 
 class BusinessCard(Card):
     """
     An individual's business card.
     """
-    def __init__(self, company_address, company_data, fname, lname, title,
+
+    def __init__(self, company_name, company_phone, company_site,
+                 company_address, fname, lname, title,
                  job_title, email, phone):
-        super().__init__(company_address, company_data)
+        super().__init__(company_name, company_phone, company_site,
+                         company_address)
         self._fname = fname
         self._lname = lname
         self._title = title
@@ -77,16 +87,23 @@ class BusinessCard(Card):
         return
 
     def __str__(self):
-        return
+        return f"===== {self._fname} {self._lname}, {self._title} =====\n" \
+               f"{self._job_title}\n" \
+               f"E-mail: {self._email}\n" \
+               f"Tel: {self._phone}\n" \
+               f"{super().__str__()}\n" \
+               f"============================="
 
 
 class TicketCard(Card):
     """
     Novelty ticket cards issued by companies.
     """
-    def __init__(self, company_address, company_data, event_date,
-                 event_time, dress_code):
-        super().__init__(company_address, company_data)
+
+    def __init__(self, company_name, company_phone, company_site,
+                 company_address, event_date, event_time, dress_code):
+        super().__init__(company_name, company_phone, company_site,
+                         company_address)
         self._event_date = event_date
         self._event_time = event_time
         self._dress_code = dress_code
@@ -95,7 +112,11 @@ class TicketCard(Card):
         return
 
     def __str__(self):
-        return
+        return f"\n======== TICKET ========\n" \
+               f"EVENT: {self._event_date} @ {self._event_time}\n" \
+               f"DRESS CODE: {self._dress_code}\n" \
+               f"{super().__str__()}\n" \
+               f"============================"
 
 
 class RewardCard(Card):
@@ -103,9 +124,12 @@ class RewardCard(Card):
     Reward card where rewards are redeemed through a type of points system
     or are of the "Buy X get Y free" stamp card format.
     """
-    def __init__(self, company_address, company_data, curr_points,
+
+    def __init__(self, company_name, company_phone, company_site,
+                 company_address, curr_points,
                  reward_list):
-        super().__init__(company_address, company_data)
+        super().__init__(company_name, company_phone, company_site,
+                         company_address)
         self._curr_points = curr_points
         self._reward_list = reward_list
 
@@ -121,8 +145,11 @@ class BalanceCard(Card):
     For any low-security card that involves a balance, such as gift
     card or transit fare card.
     """
-    def __init__(self, company_address, company_data, card_no, curr_balance):
-        super().__init__(company_address, company_data)
+
+    def __init__(self, company_name, company_phone, company_site,
+                 company_address, card_no, curr_balance):
+        super().__init__(company_name, company_phone, company_site,
+                         company_address)
         self._card_no = card_no
         self._curr_balance = curr_balance
 
@@ -138,8 +165,11 @@ class MemberCard(Card):
     Any card that is primarily used by issuing a member ID to cardholder.
     Encompasses bank cards, SIN cards, student cards, etc.
     """
-    def __init__(self, company_address, company_data, card_no, expiry_date):
-        super().__init__(company_address, company_data)
+
+    def __init__(self, company_name, company_phone, company_site,
+                 company_address, card_no, expiry_date):
+        super().__init__(company_name, company_phone, company_site,
+                         company_address)
         self._card_no = card_no
         self._expiry_date = expiry_date
 
@@ -154,9 +184,12 @@ class MoneyCard(MemberCard):
     """
     For debit and credit cards.
     """
-    def __init__(self, company_address, company_data, card_no, expiry_date,
+
+    def __init__(self, company_name, company_phone, company_site,
+                 company_address, card_no, expiry_date,
                  csv, card_type, chip_enabled, billing_address):
-        super().__init__(company_address, company_data, card_no, expiry_date)
+        super().__init__(company_name, company_phone, company_site,
+                         company_address, card_no, expiry_date)
         self._csv = csv
         self._card_type = card_type
         self._chip_enabled = chip_enabled
@@ -174,10 +207,13 @@ class MemberRewardCard(RewardCard, MemberCard):
     For more complex reward systems that issue membership IDs to cardholders.
     Includes cards such as Air Miles or Scene cards.
     """
-    def __init__(self, company_address, company_data, curr_points,
+
+    def __init__(self, company_name, company_phone, company_site,
+                 company_address, curr_points,
                  reward_list, card_no, expiry_date):
         RewardCard.__init__(curr_points, reward_list)
-        MemberCard.__init__(company_address, company_data, card_no, expiry_date)
+        MemberCard.__init__(company_name, company_phone, company_site,
+                            company_address, card_no, expiry_date)
 
     def format_json_card(self):
         return
@@ -186,3 +222,21 @@ class MemberRewardCard(RewardCard, MemberCard):
         return
 
 
+def main():
+    add = Address("555 Seymour Street", "V3K 2H1", "Vancouver", "BC", "Canada")
+    add_ghib = Address("1 Chome-1-83 Shimorenjaku", "181-0013",
+                       "Mitaka", "Tokyo", "Japan")
+    # card = Card("BCIT Downtown Campus", "604-555-2143", "www.bcit.ca", add)
+    # business_card = BusinessCard("BCIT Downtown Campus", "604-555-7890",
+    #                              "www.bcit.ca", add, "Rahul", "Kukreja",
+    #                              "Mr", "Programming Instructor",
+    #                              "rkukreja@bcit.ca", "778-555-8989")
+    ticket = TicketCard("Ghibli Museum, Mitaka", "81 570-055-777",
+                        "www.ghibli-museum.jp", add_ghib,
+                        "5/24/2020",
+                        "11:00 AM", "Casual")
+    print(ticket.jsonfy())
+    print(ticket)
+
+if __name__ == "__main__":
+    main()
--- Assignments/Assignment 2/card_management.py
+++ Assignments/Assignment 2/card_management.py
@@ -2,11 +2,20 @@
 Management side of card management system.  Includes data import / export
 and the ability for users to interact with the system.
 """
-
+import json
+from card import Card
 
 class CardManager:
-    def __init__(self, card_list):
-        self._card_list = card_list
+    def __init__(self):
+    # def __init__(self, card_list):
+    #     self._card_list = card_list
+        with open("./sample_card_data.txt", "r") as data_file:
+            self._card_list = json.load(data_file)
+
+    def start(self):
+        output = UserInterface.welcome()
+        output += UserInterface.display_start_menu()
+        print(output)
 
     def view_all_cards(self):
         return
@@ -30,6 +39,42 @@ class CardManager:
 class UserInterface:
 
     @staticmethod
-    def display_start_menu():
-        return
+    def welcome():
+        msg = "\n\n==== One-der Card ====\n" \
+              "Your one-stop solution for managing a wallet full of\n" \
+              "gift cards, credit cards, business cards, points cards,\n" \
+              "and more.\n"
+        return msg
 
+    @staticmethod
+    def display_start_menu():
+        return Catalogue.get_menu(Catalogue.start_menu)
+
+
+class Catalogue:
+    start_menu = {
+        1: "View all cards",
+        2: "View cards by type",
+        3: "Add a card",
+        4: "Delete a card",
+        5: "Export card data",
+        6: "Import card data"
+    }
+
+    @classmethod
+    def get_menu(cls, menu):
+        output = "\n==== Menu ====\n"
+        for k, v in menu.items():
+            output += f"{k}. {v}\n"
+        return output
+
+
+def main():
+    manager = CardManager()
+    # card_list = []
+    # for card in manager._card_list:
+    #     card_list.append(Card(**card))
+
+
+if __name__ == "__main__":
+    main()

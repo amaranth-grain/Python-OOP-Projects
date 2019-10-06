bd65559571fccb5358f83dccefb270cab1630614
Christy Yau <abstruse.amaranth@gmail.com>
2019-10-05 23:47:20 -0700

    Added skeleton for card management module

---
+++ Assignments/Assignment 2/card_management.py
@@ -0,0 +1,45 @@
+"""
+Management side of card management system.  Includes data import / export
+and the ability for users to interact with the system.
+"""
+
+
+class CardManager:
+    def __init__(self, card_list):
+        self._card_list = card_list
+
+    def view_all_cards(self):
+        return
+
+    def view_cards_by_type(self):
+        return
+
+    def add_card(self):
+        return
+
+    def search_card(self):
+        return
+
+    def backup_data(self):
+        return
+
+    def import_data(self):
+        return
+
+
+class UserInterface:
+
+    @staticmethod
+    def display_start_menu():
+        return
+
+
+class FileHandler:
+
+    @staticmethod
+    def import_data():
+        return
+
+    @staticmethod
+    def export_data():
+        return
